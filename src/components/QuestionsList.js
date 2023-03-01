08a974f44b7adce042d75e2fabf69b9e84a46e49
KAMALAVALLI M <106173143+Kamalavali@users.noreply.github.com>
2023-03-01 16:50:00 +0530

    Add files via upload

---
+++ src/components/QuestionsList.js
@@ -0,0 +1,47 @@
+const Questionslist=[{
+    Question: "What is the national animal of india?",
+    answersList:[
+        {answer: "Lion",isCorrect:false},
+        {answer: "Tiger",isCorrect:true},
+        {answer: "Elephant",isCorrect:false},
+        {answer: "Bear",isCorrect:false},
+    ],
+    },
+    {
+       Question: "What is the language being spoken in China?",
+       answersList:[
+        {answer: "Tamil",isCorrect:false},
+        {answer: "English",isCorrect:true},
+        {answer: "Mandarian",isCorrect:false},
+        {answer: "Chinese",isCorrect:false},
+    ],
+    },
+    {
+       Question: "What is the color that symboloses peace?",
+        answersList:[
+        {answer: "white",isCorrect:true},
+        {answer: "yellow",isCorrect:false},
+        {answer: "red",isCorrect:false},
+        {answer: "pure whitish grey",isCorrect:false},
+    ],
+    },
+    {
+        Question: "What is the short form of Hyper text markup language?",
+    answersList:[
+        {answer: "HTTP",isCorrect:false},
+        {answer: "HML",isCorrect:false},
+        {answer: "Html",isCorrect:true},
+        {answer: "HTL",isCorrect:false},
+    ],
+    },
+    {
+       Question: "Which city is called as temple city?",
+        answersList:[
+        {answer: "Amristar",isCorrect:true},
+        {answer: "Tanjavore",isCorrect:false},
+        {answer: "Mumbai",isCorrect:false},
+        {answer: "Delhi",isCorrect:false},
+    ],
+    },
+];
+export default Questionslist;
